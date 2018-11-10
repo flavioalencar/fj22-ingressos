5415e83ad884cd642f033a9c60b07d55a721f4a6
Felipe <felipentelles@gmail.com>
2018-11-10 11:45:50 -0200

    Mensagem explicando o trabalho que foi feito

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,20 +2,34 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
-	public Sessao(LocalTime horario, Filme filme, Sala sala) {
+	public Sessao(LocalTime horario, Filme filme, Sala sala){
 		this.horario = horario;
 		this.filme = filme;
-		this.sala = sala;
+		this.setSala(sala);
 	}
 	
-	public LocalTime getHorarioTermino() {
+	public LocalTime getHorarioTermino(){
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
 
@@ -27,14 +41,6 @@ public class Sessao {
 		this.id = id;
 	}
 
-	public LocalTime getHorario() {
-		return horario;
-	}
-
-	public void setHorario(LocalTime horario) {
-		this.horario = horario;
-	}
-
 	public Sala getSala() {
 		return sala;
 	}
@@ -42,13 +48,4 @@ public class Sessao {
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
-
-	public Filme getFilme() {
-		return filme;
-	}
-
-	public void setFilme(Filme filme) {
-		this.filme = filme;
-	}
-	
 }
